7daa132eae4c6e671e07b822c409617a8a6a45d0
Zhenyan Jason Wang <wangzhenyan12@hotmail.com>
2022-10-23 20:48:19 +1300

    Save data persistently using SharedPreferences
    Signed-off-by: Zhenyan Jason Wang <wangzhenyan12@hotmail.com>

--- lib/screens/setup.dart
+++ lib/screens/setup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wheely_cool_app/models/items.dart';
 
 class SetupScreen extends StatefulWidget {
@@ -11,18 +12,22 @@ class _SetupWidgetState extends State<SetupScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   var _textEditingControllersList = List<TextEditingController>();
   var _textFormFieldsList = List<Widget>();
+  var _stringIdList = List<String>();
   static const int _itemsNumber = 5;
 
   @override
   void initState() {
     super.initState();
-    for (var i = 0; i < _itemsNumber; i++) {
-      _textEditingControllersList.add(TextEditingController());
+    for (var i = 1; i <= _itemsNumber; i++) {
+      _stringIdList.add(i.toString());
+      TextEditingController controller = TextEditingController();
+      _getStringValuesSF(i.toString()).then((value) => controller.text = value);
+      _textEditingControllersList.add(controller);
       _textFormFieldsList.add(
         Container(
             padding: const EdgeInsets.all(10.0),
             child: TextFormField(
-              controller: _textEditingControllersList[i],
+              controller: controller,
               decoration: const InputDecoration(
                 hintText: "Enter option",
               ),
@@ -35,6 +40,17 @@ class _SetupWidgetState extends State<SetupScreen> {
             )),
       );
     }
+  }
+
+  Future<String> _getStringValuesSF(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String stringValue = prefs.getString(key);
+    return stringValue;
+  }
+
+  _setStringToSF(String key, String value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, value);
   }
 
   @override
@@ -65,9 +81,10 @@ class _SetupWidgetState extends State<SetupScreen> {
           if (_formKey.currentState.validate()) {
             Navigator.pushNamed(context, '/wheel');
             Provider.of<ItemsModel>(context, listen: false).removeAll();
-            for (var controller in _textEditingControllersList) {
-              Provider.of<ItemsModel>(context, listen: false)
-                  .add(controller.text);
+            for (int i = 1; i <= _itemsNumber; i++) {
+              String strOption = _textEditingControllersList[i - 1].text;
+              Provider.of<ItemsModel>(context, listen: false).add(strOption);
+              _setStringToSF(i.toString(), strOption);
             }
           }
         },
